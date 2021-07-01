c308e2f07f24932d2d7fc056fed32865b4e7329c
mirzak2001 <mirzak2015@mail.ru>
2021-07-01 18:58:20 +0300

    Add files via upload

--- widget.cpp
+++ widget.cpp
@@ -18,17 +18,23 @@ void Widget::paintEvent(QPaintEvent *event)
 {
     Q_UNUSED(event);
     QPainter painter(this);
-    painter.setPen(QPen(Qt::black, 1, Qt::SolidLine, Qt::FlatCap));
+    painter.setPen(QPen(Qt::red, 1, Qt::SolidLine, Qt::FlatCap));
 
 
-        painter.setBrush(QBrush(Qt::black, Qt::SolidPattern));
-        painter.drawEllipse(200, 50, 150, 150);
+        painter.setBrush(QBrush(Qt::red, Qt::SolidPattern));
+        painter.drawEllipse(500, 170, 150, 150);
         painter.setBrush(QBrush(Qt::white, Qt::SolidPattern));
-        painter.drawEllipse(212, 103, 45, 45);
-        painter.drawEllipse(292, 103, 45, 45);
-        painter.drawEllipse(252, 138, 45, 45);
-        painter.drawEllipse(252, 65, 45, 45);
+        painter.drawEllipse(512, 223, 45, 45);
+        painter.drawEllipse(592, 223, 45, 45);
+        painter.drawEllipse(552, 258, 45, 45);
+        painter.drawEllipse(552, 185, 45, 45);
 
 
 
 }
+
+void Widget::on_pushButton_clicked()
+{
+    this->close();
+}
+
